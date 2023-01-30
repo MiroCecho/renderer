7efecb4c638542cb4cdc7ca7e0927b7df60dcb54
Mirko <miroslav.cecho@sfera.sk>
2023-01-30 12:55:21 +0100

    prvy build

--- dist/assets/index.95ddf7c3.js
+++ dist/assets/index.95ddf7c3.js
@@ -41,8 +41,22 @@ const p = function polyfill() {
   }
 };
 p();
-console.log("AAAAA");
+const info = document.getElementById("info");
 (_a = document.getElementById("btTest")) == null ? void 0 : _a.addEventListener("click", () => {
-  alert("ahoj");
+  const URL = "https://rendererdatastorage.blob.core.windows.net/renderdata/data.json?sp=r&st=2023-01-30T08:32:17Z&se=2023-12-31T16:32:17Z&spr=https&sv=2021-06-08&sr=b&sig=D7tzNUh3UNVHCK8BYTOOwM4J8jj6D%2Bvg%2BwlTHMGnKpo%3D";
+  fetch(URL, {
+    mode: "cors",
+    method: "GET",
+    headers: {
+      "Access-Control-Allow-Origin": "https://rendererdatastorage.blob.core.windows.net",
+      "Content-Type": "application/json"
+    }
+  }).then((response) => response.json()).then(
+    (data) => {
+      info.innerText = JSON.stringify(data);
+    }
+  ).catch((e) => {
+    info.innerText = JSON.stringify(e);
+  });
 });
-//# sourceMappingURL=index.03db8d9f.js.map
+//# sourceMappingURL=index.95ddf7c3.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,21 @@
-console.log("AAAAA");
-document.getElementById("btTest")?.addEventListener("click",()=>{
-  alert("ahoj");
+const info:HTMLElement=document.getElementById("info") as any;
+
+document.getElementById("btTest")?.addEventListener("click", () => {
+   const URL = "https://rendererdatastorage.blob.core.windows.net/renderdata/data.json?sp=r&st=2023-01-30T08:32:17Z&se=2023-12-31T16:32:17Z&spr=https&sv=2021-06-08&sr=b&sig=D7tzNUh3UNVHCK8BYTOOwM4J8jj6D%2Bvg%2BwlTHMGnKpo%3D";
+  fetch(URL, {
+    mode: 'cors',
+    method: 'GET',
+    headers: {
+      "Access-Control-Allow-Origin": "https://rendererdatastorage.blob.core.windows.net",
+      'Content-Type': "application/json"
+    }
+  })
+    .then(response => response.json())
+    .then((data) => {
+      info.innerText=JSON.stringify(data);
+    }
+    ).catch(e =>{ 
+      info.innerText=JSON.stringify(e);
+    }); 
 })
+
